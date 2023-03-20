1357178fe87c0e4e5b22d4eaba3b23aca3282809
David NGAMBA <dng@afritelec.fr>
2023-03-20 17:34:53 +0100

    modification one

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const About = ()=>{
+    return ( <section>
+        <div>
+        <h1>A propos</h1>
+        <p>Pourquoi moi!!!</p>
+
+        </div>
+        
+    </section>)
+   
+}
+
+export default About;
