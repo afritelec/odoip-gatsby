323f1a52486ef6966fe549353cef3896da33db7c
David NGAMBA <dng@afritelec.fr>
2023-03-22 00:12:28 +0100

    sauv

--- src/pages/services/{mdx.frontmatter__slug}.js
+++ src/pages/services/{mdx.frontmatter__slug}.js
@@ -2,21 +2,28 @@ import * as React from 'react'
 import Layout from '../../components/layout'
 import { graphql } from 'gatsby'
 import Seo from '../../components/seo'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 const Pages = ({ data, children }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image)
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
+      
       <p>{data.mdx.frontmatter.date}</p>
       {children}
     </Layout>
   )
 }
 export const query = graphql`
-  query ($id: String) {
+  query($id: String) {
     mdx(id: {eq: $id}) {
       frontmatter {
         title
-        date(formatString: "MMMM D, YYYY")
+        date(formatString: "MMMM DD, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        
       }
     }
   }
